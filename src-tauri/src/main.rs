5d0e20d0de7114900aa717150e105101f2e2344a
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-03-21 23:51:46 +0000

    feat: remove updater plugin from main application initialization

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,14 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod commands;
 mod db;
 mod models;
 
 use tauri::Builder;
 use commands::search_medications;
-use tauri_plugin_updater;
 
 
 fn main() {
     Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![search_medications])
         .run(tauri::generate_context!())
         .expect("Error while running Tauri application");
