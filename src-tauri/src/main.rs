cc55925accc49a5091e538babc68b7c14f08a104
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-03-21 23:39:01 +0000

    feat: add updater plugin and configure desktop capabilities for application updates

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,9 +4,12 @@ mod models;
 
 use tauri::Builder;
 use commands::search_medications;
+use tauri_plugin_updater;
+
 
 fn main() {
     Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![search_medications])
         .run(tauri::generate_context!())
         .expect("Error while running Tauri application");
